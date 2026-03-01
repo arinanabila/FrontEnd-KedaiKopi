d9437371ae034638b0dfec11f6297b93938749cc
arinanabila <areenrhmania@gmail.com>
2026-03-01 13:26:31 +0700

    edit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Toggle class active
 const navbarNav = document.querySelector ('.navbar-nav');
 //ketika humburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
+    e.preventDefault();
 };
 
 //klik diluar sidebar untuk menghilangkan nav
